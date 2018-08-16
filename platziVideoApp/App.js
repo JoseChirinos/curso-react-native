46f32560d4e10f18f39d89e83575e9330d00822f
JoseChirinos <josealexcba@gmail.com>
2018-08-16 15:19:16 -0400

    new structure files added

--- platziVideoApp/App.js
+++ platziVideoApp/App.js
@@ -1,66 +1,26 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View,
-  Image
+  Text
 } from 'react-native';
+import Home from './src/screens/containers/home';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Image 
-          source={require('./assets/logo.png')}
-          style={{ width:300, height: 90}}
-        />
-        <Text style={styles.welcome}>
-          @JoseChirinos
+      <Home>
+        <Text>
+          Header
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
+        <Text>
+          Search
         </Text>
-        <Text style={styles.instructions}>
-          {instructions}
+        <Text>
+          Categories
         </Text>
-      </View>
+        <Text>
+          Suggestions
+        </Text>
+      </Home>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: Platform.select({
-      ios: 'green',
-      android: 'blue',
-    }),
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
